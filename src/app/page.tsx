1f85869ae9b3f069636d49953bdd635c4ffa3ce2
Alpharion <alphahong@gmail.com>
2026-05-18 23:01:33 +0800

    Added Login Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-[#F9F9F9] font-sans">
       <h1 className="text-4xl font-bold">Waypoint</h1>
       <h2 className="text-black mt-2">Plan the perfect trip!</h2>
-      <Link href="/new-trip">
+      <Link href="/login">
         <button className="bg-red-500 text-white px-6 py-3 rounded-full mt-7">Start Planning</button>
       </Link>
     </div>

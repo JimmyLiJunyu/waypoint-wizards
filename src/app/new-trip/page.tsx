253987116b6b6b709e0b93650b682b45a6ffdb45
JimmyIsAwesome12 <jimmyisawesome4@gmail.com>
2026-05-18 13:01:08 +0800

    imported shadcn library and implemented TripForm in new-trip page

--- src/app/new-trip/page.tsx
+++ src/app/new-trip/page.tsx
@@ -1,7 +1,10 @@
+import TripForm from "@/components/TripForm";
+
 function NewTrip() {
     return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
+    <main className="min-h-screen flex flex-col items-center justify-start pt-[20vh]">
         <h1 className="text-4xl font-bold">Plan a new trip</h1>
+        <TripForm />
     </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-[#FFFDD0] font-sans">
-      <h1>Waypoint</h1>
-      <h2>Plan the perfect trip!</h2>
+    <div className="flex flex-col flex-1 items-center justify-center bg-[#F9F9F9] font-sans">
+      <h1 className="text-4xl font-bold">Waypoint</h1>
+      <h2 className="text-black mt-2">Plan the perfect trip!</h2>
       <Link href="/new-trip">
         <button className="bg-red-500 text-white px-6 py-3 rounded-full mt-7">Start Planning</button>
       </Link>

a54a7a60bfee66776b7e0e548079151a195075bf
Jimmy <jimmyisawesome4@gmail.com>
2026-05-18 11:48:39 +0800

    added new-trip page

---
+++ src/app/new-trip/page.tsx
@@ -0,0 +1,9 @@
+function NewTrip() {
+    return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Plan a new trip</h1>
+    </main>
+    )
+}
+
+export default NewTrip;
